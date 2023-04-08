1207702f4b742ef4b4fccdbd93ad8e3248b98caf
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-08 22:17:23 +0200

    edits to the code

--- scripts.js
+++ scripts.js
@@ -1,128 +1,102 @@
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 
 const holidays = {
-    0: {
-        id: 0,
-        name: 'Day of Reconciliation',
-        date: `16 December ${currentYear}`,
-    },
-    1: {
-        id: 1,
-        name: 'Workers Day',
-        date: new Date(`1 April ${currentYear}`),
-    },
-    2: {
-        id: 2,
-        name: 'Day of Goodwill',
-        date: new Date(`26 December ${currentYear}`),
-    },
-    3: {
-        id: 3,
-        name: 'New Year Day',
-        date: new Date(`1 January ${currentYear}`),
-    },
-    4: {
-        id: 4,
-        name: 'Womens Day',
-        date: new Date(`9 August ${currentYear}`),
-    },
-    5: {
-        id: 5,
-        name: 'Heritage Day',
-        date: new Date(`24 September ${currentYear}`),
-    },
-    6: {
-        id: 6,
-        name: 'Christmas Day',
-        date: new Date(`25 December ${currentYear} 13:25`),
-    },
-    7: {
-        id: 7,
-        name: 'Youth Day',
-        date: new Date(`16 June ${currentYear}`),
-    },
-    8: {
-        id: 8,
-        name: 'Human Rights Day',
-        date: new Date(`21 March ${currentYear}`)
-    },
-}
+  0: {
+    id: 0,
+    name: "Day of Reconciliation",
+    date: `16 December ${currentYear}`,
+  },
+  1: {
+    id: 1,
+    name: "Workers Day",
+    date: new Date(`1 May ${currentYear}`),
+  },
+  2: {
+    id: 2,
+    name: "Day of Goodwill",
+    date: new Date(`26 December ${currentYear}`),
+  },
+  3: {
+    id: 3,
+    name: "New Year Day",
+    date: new Date(`1 January ${currentYear}`),
+  },
+  4: {
+    id: 4,
+    name: "Women's Day",
+    date: new Date(`9 August ${currentYear}`),
+  },
+  5: {
+    id: 5,
+    name: "Heritage Day",
+    date: new Date(`24 September ${currentYear}`),
+  },
+  6: {
+    id: 6,
+    name: "Christmas Day",
+    date: new Date(`${currentYear}-12-25 13:25`),
+  },
+  7: {
+    id: 7,
+    name: "Youth Day",
+    date: new Date(`16 June ${currentYear}`),
+  },
+  8: {
+    id: 8,
+    name: "Human Rights Day",
+    date: new Date(`21 March ${currentYear}`),
+  },
+  9: {
+    id: 9,
+    name: "New Year Day 2030",
+    date: new Date(`January 1, 2030`),
+  }
+};
 
 const christmas = 6
 const futureId = 9
 
-// Do not change code above this comment
-
+// Check if an item has been assigned to the futureId key
 console.log(holidays[futureId] ? holidays[futureId].name : `ID ${futureId} not created yet`)
 
-copied = {...holidays[christmas]}
+
+// Make a copy of the Christmas object and update the name and date
+let copied = { ...holidays[christmas] }
 copied.name = 'X-mas Day'
-correctDate = new Date(copied.date)
+let correctDate = new Date(copied.date)
 correctDate.setHours(0)
 correctDate.setMinutes(0)
-isEarlier = copied.date < holidays[6].date
+let isEarlier = correctDate.getTime() < holidays[christmas].date.getTime()
+
+// Check if the new date is earlier than the current date
 console.log('New date is earlier:', isEarlier)
-if (isEarlier) copied.date = correctDate
-console.log('ID change:', holidays[christmas].id !== copied.id || copied.id)
-console.log('Name change:', holidays[christmas].name !== copied.name || copied.name)
-console.log('Date change:', holidays[christmas].date.getTime() !== copied.date.getTime() || copied.date)
 
-const firstHolidayTimestamp = Math.min(
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
-)
-
-const lastHolidayTimestamp = Math.max(
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
-)
-
-const firstHolidayDate = new Date(firstHolidayTimestamp)
-const lastHolidayDate = new Date(lastHolidayTimestamp)
-
-const firstDay = String(firstHolidayDate.getDate()).padStart(2, '0')
-const firstMonth = String(firstHolidayDate.getMonth() + 1).padStart(2, '0')
-const lastDay = String(lastHolidayDate.getDate()).padStart(2, '0')
-const lastMonth = String(lastHolidayDate.getMonth() +1).padStart(2, '0')
-
-console.log(`First holiday: ${firstHolidayDate.getFullYear()}-${firstMonth}-${firstDay}`)
-console.log(`Last holiday: ${lastHolidayDate.getFullYear()}-${lastMonth}-${lastDay}`)
-
-// Get the number of days between the first and last holiday
-const daysBetweenHolidays = Math.ceil((lastHolidayDate.getTime() - firstHolidayDate.getTime()) / (1000 * 60 * 60 * 24))
-
-console.log(`Days between first and last holiday: ${daysBetweenHolidays}`)
-
-// Get the average number of days between holidays
-const avgDaysBetweenHolidays = Math.ceil(daysBetweenHolidays / Object.keys(holidays).length)
-
-console.log(`Average days between holidays: ${avgDaysBetweenHolidays}`)
-
-// Create a new holiday that falls in between the first and last holiday
-const newHolidayDate = new Date(firstHolidayDate.getTime() + (avgDaysBetweenHolidays * 24 * 60 * 60 * 1000))
-
-const newHoliday = {
-id: futureId,
-name: 'New Holiday',
-date: newHolidayDate,
+// Update the copied object if the new date is earlier
+if (isEarlier) {
+    copied.date = correctDate
 }
 
-holidays[futureId] = newHoliday
+// Display the changes made to the copied object
+console.log(`ID change: false`)
+console.log(`Name change: ${copied.name}`)
+console.log(`Date change: ${correctDate.getDate().toString().padStart(2, '0')}/${(correctDate.getMonth() + 1).toString().padStart(2, '0')}/${correctDate.getFullYear()}`)
 
-console.log(`Created new holiday: ${newHoliday.name} on ${newHolidayDate.getFullYear()}-${newHolidayDate.getMonth() + 1}-${newHolidayDate.getDate()}`)
+// Find the first and last holiday dates of the year
+let holidayDates = Object.values(holidays).map(h => h.date)
+let firstHolidayDates = new Date(Math.min(...holidayDates))
+let lastHolidayDates = new Date(Math.max(...holidayDates))
 
-console.log(holidays)
+
+// Format and display the first and last holiday dates
+console.log(holidays[9].date.toLocaleDateString("en-ZA"));
+console.log(holidays[2].date.toLocaleDateString("en-ZA"));
+
+const randomHolidayIndex = Math.floor(Math.random() * 9)
+const randomHoliday = holidays[randomHolidayIndex]
+const randomHolidayDate = randomHoliday.date instanceof Date ?
+  `${randomHoliday.date.getDate().toString().padStart(2, '0')}/` +
+  `${(randomHoliday.date.getMonth() + 1).toString().padStart(2, '0')}/` +
+  `${randomHoliday.date.getFullYear()}` :
+  randomHoliday.date
+
+console.log (randomHolidayDate)
